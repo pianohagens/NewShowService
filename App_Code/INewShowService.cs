99880478a5e84ee54f3a1c51493f57b256c2a234
pianohagens <pianohagens@gmail.com>
2016-03-06 20:14:51 -0800

    NewShowService
    It works.

---
+++ App_Code/INewShowService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "INewShowService" in both code and config file together.
+[ServiceContract]
+public interface INewShowService
+{
+    [OperationContract]
+    List<Show> GetShowsByArtist(string artistName);
+     
+}
+ [DataContract]
+public class ShowInfo
+{
+    [DataMember]
+    public string ArtistName { get; set; }
+    [DataMember]
+    public string ShowName { get; set; }
+    [DataMember]
+    public string ShowDate { get; set; }
+    [DataMember]
+    public string ShowTime { get; set; }
+
+    [DataMember]
+    public string TicketInfo { get; set; }
+}
---
+++ App_Code/NewShowService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "NewShowService" in code, svc and config file together.
+public class NewShowService : INewShowService
+{
+    ShowTrackerEntities ns = new ShowTrackerEntities();
+    private List<Show> Shows;
+
+    public List<Show> GetShowsByArtist(string artistName)
+    {
+        var shws = from s in ns.Shows
+                   from d in s.ShowDetails
+                   where artistName.Equals(artistName)
+                   select new
+                   {
+                       d.Artist.ArtistName,
+                       s.ShowName,
+                       s.ShowTime,
+                       s.ShowDate,
+                       s.ShowTicketInfo
+
+                   };
+        List<ShowInfo> shows = new List<ShowInfo>();
+
+        foreach (var sh in shws)
+        {
+            ShowInfo sInfo = new ShowInfo();
+            sInfo.ArtistName = sh.ArtistName;
+            sInfo.ShowName = sh.ShowName;
+            sInfo.ShowDate = sh.ShowDate.ToShortDateString();
+            sInfo.ShowTime = sh.ShowTime.ToString();
+            shows.Add(sInfo);
+        }
+
+        return Shows;
+    }
+
+
+
+}
